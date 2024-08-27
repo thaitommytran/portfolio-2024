ea06fddf0ad129a423813198440a4bb2824b6cfd
Thai Tran <thai.tommy.tran@gmail.com>
2024-08-27 16:57:00 -0500

    added experience section

--- script.js
+++ script.js
@@ -4,6 +4,63 @@ const downloadLink = document.querySelector("button.btn.btn-color-2");
 const contactInfo = document.querySelector("button.btn.btn-color-1");
 const linkedInProfile = document.querySelector("img[alt='LinkedIn Profile']");
 const gitHubProfile = document.querySelector("img[alt='GitHub Profile']");
+const experienceContainer = document.querySelector(".experience-containers");
+
+const frontend = {
+  HTML: "Experienced",
+  CSS: "Experienced",
+  JavaScript: "Intermediate",
+  React: "Intermediate",
+  TypeScript: "Basic",
+  "Tailwind CSS": "Basic"
+};
+
+const backend = {
+  NodeJS: "Intermediate",
+  ExpressJS: "Basic",
+  PostgreSQL: "Basic",
+  Git: "Intermediate",
+  Firebase: "Basic"
+};
+
+function getExperience(development) {
+  const devType = () => {
+    if (development == frontend) return "Frontend";
+    return "Backend";
+  };
+
+  const details = document.createElement("div");
+  const subTitle = document.createElement("h2");
+  const articleContainer = document.createElement("div");
+
+  details.classList.add("details-container");
+  subTitle.classList.add("experience-sub-title");
+  articleContainer.classList.add("article-container");
+
+  details.appendChild(subTitle);
+  details.appendChild(articleContainer);
+
+  subTitle.innerText = `${devType()} Development`;
+
+  for (const skill in development) {
+    const level = development[skill];
+    const article = document.createElement("article");
+
+    article.innerHTML = `            
+      <img
+        class="icon"
+        src="assets/checkmark.png"
+        alt="Experience Icon" />
+      <div>
+        <h3>${skill}</h3>
+        <p>${level}</p>
+      </div>`;
+
+    articleContainer.appendChild(article);
+  }
+
+  experienceContainer.appendChild(details);
+}
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
@@ -35,3 +92,6 @@ downloadLink.addEventListener("click", openResume);
 contactInfo.addEventListener("click", scrollToContact);
 linkedInProfile.addEventListener("click", openLinkedIn);
 gitHubProfile.addEventListener("click", openGitHub);
+
+getExperience(frontend);
+getExperience(backend);
